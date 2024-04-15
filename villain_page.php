b7612d0f32f88a09e72f896fe40738594ae46d7d
Luke Blair <lblair@umw.edu>
2024-04-15 11:32:54 -0400

    added a file

---
+++ villain_page.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Culmination Project</title>
+        <link rel = "stylesheet" href="stylesheet.css">
+        <script src="script.js"></script>
+
+        <?php
+                $servername = "localhost";
+                $DB = "Team_Panther";
+                include "credentials.php";
+                $connection = mysqli_connect($servername, $username, $password, $DB);
+
+                if(mysqli_connect_errno()) {
+                    echo "Failed to connect to server";
+                } else {
+                    echo "Connection succesful";
+                }
+            ?>
+    </head>
+    <body>
+<div class="image-container">
+  <div class="text">VILLAINS</div>
+</div>
+    </body>
+</html>
+
