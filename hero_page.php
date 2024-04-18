39e94bcdf5365db1252060af795319ac805a325e
Israel Ortiz <israeljortiz16@gmail.com>
2024-04-18 17:15:11 -0400

    Added page design elements

--- hero_page.php
+++ hero_page.php
@@ -22,9 +22,73 @@
                 }
             ?>
     </head>
-    <body>
-<div class="image-container">
-  <div class="text">HEROES</div>
+
+<div id="navbar">
+  <a href="#default" id="logo">WebpageLogo</a>
+  <div id="navbar-right">
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/home_page.php">Home</a>
+    <a class="active" href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/hero_page.php">Heroes</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/villain_page.php">Villains</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/neutral_page.php">Neutrals</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/about_page.html">About</a>
+  </div>
 </div>
+
+
+<script>
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if ((document.body.scrollTop > 80 && document.body.scrollTop < 500) || (document.documentElement.scrollTop > 80 && document.documentElement.scrollTop < 500)) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+    document.getElementById("logo").style.fontSize = "18px";
+    document.getElementById("navbar").style.top = "0";
+  } else if (document.body.scrollTop > 499 || document.documentElement.scrollTop > 499) {
+    document.getElementById("navbar").style.padding = "0px 0px";
+    document.getElementById("logo").style.fontSize = "0px";
+    document.getElementById("navbar").style.top = "-50px"
+  } else {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.top = "0";
+  }
+}
+</script>
+
+    <body>
+<!--<div class="image-container">
+  <div class="text">HEROES</div>
+</div>-->
+<div class="slideshow-container">
+
+<div class="mySlides">
+  <q>I am Iron Man</q>
+  <p class="author">- You know who</p>
+</div>
+
+<div class="mySlides">
+  <q>Avengers, assemble.</q>
+  <p class="author">- America's Ass</p>
+</div>
+
+<div class="mySlides">
+  <q>With great power comes great responsibility</q>
+  <p class="author">- Some old dude</p>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+
+<div class="dot-container">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
+
+    <body>
     </body>
 </html>
--- home_page.php
+++ home_page.php
@@ -26,17 +26,113 @@
 <div id="navbar">
   <a href="#default" id="logo">WebpageLogo</a>
   <div id="navbar-right">
-    <a href="#about">About</a>
+    <a class="active" href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/home_page.php">Home</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/hero_page.php">Heroes</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/villain_page.php">Villains</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/neutral_page.php">Neutrals</a>
+    <a href="https://cpsc.umw.edu/Team_Panther/CPSC350_Final_Project/about_page.html">About</a>
   </div>
 </div>
 
-<div style="margin-top:210px;padding:15px 15px 2500px;font-size:30px">
-  <p><b>This ALTERED example demonstrates how to shrink a navigation bar when the user starts to scroll the page. I altered the script slightly. </b></p>
-  <p>Scroll down this frame to see the effect!</p>
-  <p>Scroll to the top to remove the effect.</p>
-  <p><b>Note:</b> We have also made the navbar responsive, resize the browser window to see the effect.</p>
-  <p>Lorem ipsum dolor dummy text to enable scrolling, sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<img src="team.png" alt="layironman">
+<div class="slideshow-container">
+
+<div class="mySlides">
+  <q>a person who is admired or idealized for courage, outstanding achievements, or noble qualities</q>
+  <p class="author">- Oxford</p>
 </div>
+
+<div class="mySlides">
+  <q> a mythological or legendary figure often of divine descent endowed with great strength or ability</q>
+  <p class="author">- Webster</p>
+</div>
+
+<div class="mySlides">
+  <q>another term for submarine</q>
+  <p class="author">- No really, google it</p>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+
+<div class="dot-container">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+<h1>Testa</h1>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+<p>Hoagie Fest</p>
+
+
 
 <script>
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
@@ -46,15 +142,15 @@ function scrollFunction() {
   if ((document.body.scrollTop > 80 && document.body.scrollTop < 500) || (document.documentElement.scrollTop > 80 && document.documentElement.scrollTop < 500)) {
     document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "18px";
-    document.getElementById("navbar").style.width = "100%";
+    document.getElementById("navbar").style.top = "0";
   } else if (document.body.scrollTop > 499 || document.documentElement.scrollTop > 499) {
     document.getElementById("navbar").style.padding = "0px 0px";
     document.getElementById("logo").style.fontSize = "0px";
-    document.getElementById("navbar").style.width = "0%";
+    document.getElementById("navbar").style.top = "-50px"
   } else {
     document.getElementById("navbar").style.padding = "20px 10px";
     document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("navbar").style.width = "100%";
+    document.getElementById("navbar").style.top = "0";
   }
 }
 </script>
